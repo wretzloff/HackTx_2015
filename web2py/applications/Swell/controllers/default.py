61fb0eebb703184bf923d65e8e6722548561b8ce
Will Retzloff <wretzloff@gmail.com>
2015-09-26 19:00:50 -0500

    Initial logic in the Index controller.

--- web2py/applications/Swell/controllers/default.py
+++ web2py/applications/Swell/controllers/default.py
@@ -6,13 +6,21 @@ def index():
     logged_in_user_id = loggedInUserResults[0].id
     
     #Use the ID of the logged in user to get a list of devices that the user has authenticated with.
-    credentialsQuery = (db.credentials.account == logged_in_user_id)
-    credentialsResults = db(credentialsQuery).select()
     devicesList = []
-    for credentialsRow in credentialsResults:
+    credentialsRows = db(db.credentials.account == logged_in_user_id).select(join=db.resourceOwners.on(db.credentials.resourceOwner==db.resourceOwners.id))
+    for row in credentialsRows:
+        devicesList.append(row.resourceOwners.resourceOwnerName)
+        
+        
+    
+    #credentialsQuery = (db.credentials.account == logged_in_user_id)
+    #credentialsResults = db(credentialsQuery).select()
+    #devicesList = []
+    #for credentialsRow in credentialsResults:
         #Get the text of the email
-        resourceOwnerName = credentialsRow.resourceOwner
-        devicesList.append(resourceOwnerName)
+    #    resourceOwnerName = credentialsRow.resourceOwner
+    #    devicesList.append(resourceOwnerName)
+    
     
     return dict(message=T('Hello World'), devicesList = devicesList)
 
